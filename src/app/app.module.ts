a327ff1d87e54d69497a8be6f6455d1679edeb93
Gthalles <thalles@gempe.dev>
2021-08-18 20:21:38 -0300

    Estilização de alguns compoentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgIfDirectiveComponent } from './ng-if-directive/ng-if-directive.component';
 import { NgSwitchDirectiveComponent } from './ng-switch-directive/ng-switch-directive.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
