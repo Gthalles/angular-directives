507109714d8540643e828f596d02cc46f448c697
Gthalles <thalles@gempe.dev>
2021-08-19 12:04:21 -0300

    Aula 30 - Exemplos de mudança de estilo de componentes  utilizando a diretiva ngStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NgIfDirectiveComponent } from './ng-if-directive/ng-if-directive.component';
 import { NgSwitchDirectiveComponent } from './ng-switch-directive/ng-switch-directive.component';
 import { NgClassDirectiveComponent } from './ng-class-directive/ng-class-directive.component';
+import { NgStyleComponentComponent } from './ng-style-component/ng-style-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgIfDirectiveComponent,
     NgSwitchDirectiveComponent,
-    NgClassDirectiveComponent
+    NgClassDirectiveComponent,
+    NgStyleComponentComponent
   ],
   imports: [
     BrowserModule
